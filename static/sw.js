3bca2755d2cd78f244a68657bfb13e5998fa0c9e
rodriguez-bit <rodriguez@dajanarodriguez.com>
2026-03-06 11:48:05 +0100

    feat: add file upload tab - import audio recordings from external apps

--- static/sw.js
+++ static/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ridea-v2.8.0';
+const CACHE_NAME = 'ridea-v2.9.0';
 const urlsToCache = ['/recorder', '/static/recorder.html', '/static/icon-192.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(urlsToCache)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))).then(() => { self.clients.matchAll().then(cls => cls.forEach(c => c.postMessage({type:'SW_UPDATED'}))); return self.clients.claim(); })); });
